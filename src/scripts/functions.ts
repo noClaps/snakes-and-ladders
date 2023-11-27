d17ff994ec60fdf95e07bd0869138ff9ef2aa4bd
noClaps <github@zerolimits.dev>
2023-11-27 00:06:42 +0000

    Combine snakes and ladders into one layer

--- src/scripts/functions.ts
+++ src/scripts/functions.ts
@@ -10,86 +10,87 @@ export function roll(player: number) {
     return [newPosition, rollDice];
 }
 
-export class Ladders {
+export class SnakesAndLadders {
     constructor () {
-        this.#ladders = this.#generateLadders();
-    }
+        let { snakes, ladders } = this.#generate();
 
-    #ladders: number[][];
-
-    #generateLadders() {
-        const generateLadder = () => [Math.round(Math.random() * 97) + 2, Math.round(Math.random() * 97) + 2];
-        const hasDuplicate = (arr: number[][]) => arr.flatMap(i => i).length !== new Set(arr.flatMap(i => i)).size;
-        const sameRow = (arr: number[][]) => arr.find(ladder => Math.floor(ladder[1] / 10) - Math.floor(ladder[0] / 10) === 0);
-
-        const ladders: number[][] = [];
-
-        while (ladders.length < 5) {
-            const ladder = generateLadder();
-            ladder.sort((a, b) => a - b);
-
-            const tempLadder = [...ladders];
-            tempLadder.push(ladder);
-
-            if (!hasDuplicate(tempLadder) && !sameRow(tempLadder)) {
-                ladders.push(ladder);
-            }
+        while (this.#samePos(snakes, ladders)) {
+            const { snakes: newSnakes, ladders: newLadders } = this.#generate();
+            snakes = newSnakes;
+            ladders = newLadders;
         }
 
-        return ladders;
-    }
-
-    get() {
-        return this.#ladders;
-    }
-    reset() {
-        this.#ladders = this.#generateLadders();
-    }
-}
-
-export class Snakes {
-    constructor () {
-        this.#snakes = this.#generateSnakes();
+        this.#snakes = snakes;
+        this.#ladders = ladders;
     }
 
     #snakes: number[][];
+    #ladders: number[][];
 
-    #generateSnakes() {
-        const generateSnake = () => [Math.round(Math.random() * 97) + 2, Math.round(Math.random() * 97) + 2];
+    #generate() {
+        const generatePair = () => [Math.round(Math.random() * 97) + 2, Math.round(Math.random() * 97) + 2];
         const hasDuplicate = (arr: number[][]) => arr.flatMap(i => i).length !== new Set(arr.flatMap(i => i)).size;
-        const sameRow = (arr: number[][]) => arr.find(snake => Math.floor(snake[1] / 10) - Math.floor(snake[0] / 10) === 0);
+        const sameRow = (arr: number[][]) => arr.find(el => Math.floor(el[1] / 10) - Math.floor(el[0] / 10) === 0);
 
         const snakes: number[][] = [];
+        const ladders: number[][] = [];
+
 
         while (snakes.length < 5) {
-            const snake = generateSnake();
+            const snake = generatePair();
             snake.sort((a, b) => b - a);
 
-            const tempSnake = [...snakes];
-            tempSnake.push(snake);
+            const tempSnakes = [...snakes];
+            tempSnakes.push(snake);
 
-            if (!hasDuplicate(tempSnake) && !sameRow(tempSnake)) {
+            if (!hasDuplicate(tempSnakes) && !sameRow(tempSnakes)) {
                 snakes.push(snake);
             }
         }
 
-        return snakes;
+
+        while (ladders.length < 5) {
+            const ladder = generatePair();
+            ladder.sort((a, b) => a - b);
+
+            const tempLadders = [...ladders];
+            tempLadders.push(ladder);
+
+            if (!hasDuplicate(tempLadders) && !sameRow(tempLadders)) {
+                ladders.push(ladder);
+            }
+        }
+
+
+        return { snakes, ladders };
+    }
+
+    #samePos(snakes: number[][], ladders: number[][]) {
+        const flatLadders = ladders.flatMap(i => i);
+        const flatSnakes = snakes.flatMap(i => i);
+
+        const combined = [...flatLadders, ...flatSnakes];
+
+        return combined.length !== new Set(combined).size;
     }
 
     get() {
-        return this.#snakes;
+        return {
+            snakes: this.#snakes,
+            ladders: this.#ladders
+        };
     }
 
     reset() {
-        this.#snakes = this.#generateSnakes();
+        let { snakes, ladders } = this.#generate();
+
+        while (this.#samePos(snakes, ladders)) {
+            const { snakes: newSnakes, ladders: newLadders } = this.#generate();
+            snakes = newSnakes;
+            ladders = newLadders;
+        }
+
+        this.#snakes = snakes;
+        this.#ladders = ladders;
     }
-}
-
-export function samePos(ladders: number[][], snakes: number[][]) {
-    const flatLadders = ladders.flatMap(i => i);
-    const flatSnakes = snakes.flatMap(i => i);
-
-    const combined = [...flatLadders, ...flatSnakes]
-    
-    return combined.length !== new Set(combined).size
 }
